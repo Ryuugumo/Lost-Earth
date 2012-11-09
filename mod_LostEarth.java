78cf097eac822fec5801f60bd1118fe28620df1f
Ryuugumo <ryuugumo@hotmail.com>
2012-11-08 20:15:20 -0500

    Added Tools and Sword for Mithral
    Armor implemented but won't equip with Mithral Stats
    temporarily reverted back to Diamond Stats

--- mod_LostEarth.java
+++ mod_LostEarth.java
@@ -1,6 +1,6 @@
 //TODO fix scaffold to act like a ladder
-//TODO fix tamper to have a config file
-//TODO add uses for mithral ingots
+//TODO fix tamper to have a config file for gravel(0), cobblestone(1), stonebrick(2)
+//TODO fix MithralArmor to be called through EnumArmorMaterialLE.MITHRAL and not EnumArmorMaterial.DIAMOND
 package net.minecraft.src;
 
 import java.util.Random;
@@ -36,13 +36,23 @@ public class mod_LostEarth extends BaseMod
 	public static final Block blockthatchslabB = (new BlockThatchSlab(707, true)).setHardness(0.5F).setResistance(1F).setBlockName("blockthatchslabB").setCreativeTab(CreativeTabs.tabBlock);
 	public static final Block blockwaddlendab = (new BlockWaddleNDab(708, 0)).setHardness(1F).setResistance(2F).setBlockName("blockwaddlendab").setCreativeTab(CreativeTabs.tabBlock);
 	public static final Block blockshoji = (new BlockShoji(709, 0, 0, Material.glass, false)).setHardness(0.1F).setResistance(1F).setBlockName("blockshoji").setCreativeTab(CreativeTabs.tabDecorations);
-	//Item
+	//Items
 	public static final Item PungiItem = (new ItemPungi(BlockPungi -256, blockpungi)).setItemName("ItemPungi");
 	public static final Item ScaffoldItem = (new ItemScaffold(BlockScaffold -256, blockscaffold)).setItemName("ItemScaffold");
 	public static final Item ChandelierItem = (new ItemChandelier(BlockChandelier -256, blockchandelier)).setItemName("ItemChandelier");
 	public static final Item LanternItem = (new ItemLantern(BlockLantern -256, blocklantern)).setItemName("ItemLantern");	
 	public static final Item Tamper = (new ItemTamper(710)).setItemName("Tamper");
 	public static final Item ingotmithral = (new ItemIngotMithral(711)).setItemName("ingotmithral").setCreativeTab(CreativeTabs.tabMaterials);
+	//Tools, Weapons, Armor
+	public static final Item MSpade = new ItemSpadeLE(730, EnumToolMaterialLE.MITHRAL).setItemName("MSpade");
+	public static final Item MHoe = new ItemHoeLE(731, EnumToolMaterialLE.MITHRAL).setItemName("MHoe");
+	public static final Item MAxe = new ItemAxeLE(732, EnumToolMaterialLE.MITHRAL).setItemName("MAxe");
+	public static final Item MPickaxe = new ItemPickaxeLE(733, EnumToolMaterialLE.MITHRAL).setItemName("MPickaxe");
+	public static final Item MSword = new ItemSwordLE(734, EnumToolMaterialLE.MITHRAL).setItemName("MSword");
+	public static final Item MHead = new ItemArmor(735, EnumArmorMaterial.DIAMOND, ModLoader.addArmor("mithral"), 0).setItemName("MHead");
+	public static final Item MChest = new ItemArmor(736, EnumArmorMaterial.DIAMOND, ModLoader.addArmor("mithral"), 1).setItemName("MChest");
+	public static final Item MLegs = new ItemArmor(737, EnumArmorMaterial.DIAMOND, ModLoader.addArmor("mithral"), 2).setItemName("MLegs");
+	public static final Item MFeet = new ItemArmor(738, EnumArmorMaterial.DIAMOND, ModLoader.addArmor("mithral"), 3).setItemName("MFeet");
 	//MultiSidedBlocks
 	public static int ThatchSide;
 	public static int ThatchEnd;
@@ -61,6 +71,15 @@ public class mod_LostEarth extends BaseMod
 		blockshoji.blockIndexInTexture = ModLoader.addOverride("/terrain.png", "/LE/BlockShoji.png");
 		Tamper.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/ItemTamper.png");
 		ingotmithral.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/ItemIngotMithral.png");
+		MSpade.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MSpade.png");
+		MHoe.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MHoe.png");
+		MAxe.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MAxe.png");
+		MPickaxe.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MPickaxe.png");
+		MSword.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MSword.png");
+		MHead.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MHead.png");
+		MChest.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MChest.png");
+		MLegs.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MLegs.png");
+		MFeet.iconIndex = ModLoader.addOverride("/gui/items.png", "/LE/MFeet.png");
 		//ModLoader.registerBlock()
 		ModLoader.registerBlock(blockoremithral);
 		ModLoader.registerBlock(blockthatch);
@@ -83,6 +102,15 @@ public class mod_LostEarth extends BaseMod
 		ModLoader.addName(blockshoji, "Shoji");
 		ModLoader.addName(Tamper, "Tamper");
 		ModLoader.addName(ingotmithral, "Mithral Ingot");
+		ModLoader.addName(MSpade, "Mithral Shovel");
+		ModLoader.addName(MHoe, "Mithral Hoe");
+		ModLoader.addName(MAxe, "Mithral Axe");
+		ModLoader.addName(MPickaxe, "Mithral Pickaxe");
+		ModLoader.addName(MSword, "Mithral Sword");
+		ModLoader.addName(MHead, "Mithral Helmet");
+		ModLoader.addName(MChest, "Mithral Breastplate");
+		ModLoader.addName(MLegs, "Mithral Leggings");
+		ModLoader.addName(MFeet, "Mithral Boots");
 		//ModLoader.addRecipe(,)
 		ModLoader.addRecipe(new ItemStack(blockpungi, 1), new Object[]
 				{"XYX","XXX","ZZZ",'X', Item.stick, 'Y', Item.spiderEye, 'Z', Block.planks});
@@ -108,6 +136,24 @@ public class mod_LostEarth extends BaseMod
 				{"YXY","XXX","YXY", 'X', Item.paper, 'Y', Item.stick});
 		ModLoader.addRecipe(new ItemStack(Tamper, 1),new Object[]
 				{" X "," X ","YYY",'X', Item.stick, 'Y', Block.stone});
+		ModLoader.addRecipe(new ItemStack(MSpade, 1), new Object[]
+				{" X "," Y "," Y ", 'X', ingotmithral, 'Y', Item.stick});
+		ModLoader.addRecipe(new ItemStack(MHoe, 1), new Object[]
+				{"XX "," Y "," Y ", 'X', ingotmithral, 'Y', Item.stick});
+		ModLoader.addRecipe(new ItemStack(MAxe, 1), new Object[]
+				{"XX ","XY "," Y ", 'X', ingotmithral, 'Y', Item.stick});
+		ModLoader.addRecipe(new ItemStack(MPickaxe, 1), new Object[]
+				{"XXX"," Y "," Y ", 'X', ingotmithral, 'Y', Item.stick});
+		ModLoader.addRecipe(new ItemStack(MSword, 1), new Object[]
+				{" X "," X "," Y ", 'X', ingotmithral, 'Y', Item.stick});
+		ModLoader.addRecipe(new ItemStack(MHead, 1), new Object[]
+				{"XXX","X X", 'X', ingotmithral});
+		ModLoader.addRecipe(new ItemStack(MChest, 1), new Object[]
+				{"X X","XXX","XXX", 'X', ingotmithral});
+		ModLoader.addRecipe(new ItemStack(MLegs, 1), new Object[]
+				{"XXX","X X","X X", 'X', ingotmithral});
+		ModLoader.addRecipe(new ItemStack(MFeet, 1), new Object[]
+				{"X X","X X", 'X', ingotmithral});
 		//recipe.crafting.shapeless
 
 		//recipe.smelting
